24d76868127749e23bf7e6e8ee7e6b9161c2668e
Emi Volpe <emiv2000@gmsil.com>
2019-02-27 10:45:04 -0600

    frst commit

---
+++ javascript/game.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<link rel="stylesheet" type="text/css" href="style.css"/>
+
+<html lang="en-us">
+
+    <body>
+
+        <script type="text/javascript">
+    
+        </script>
+
+    </body>
+
+
+</html>
